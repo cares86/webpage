6445b016665d3084e8544f8a40884d7d5fca6234
caressa van groenenbergh <cares86@MacBook-Pro-van-caressa.local>
2017-11-13 08:06:39 +0100

    voor nu weer geupdate

--- code.js
+++ code.js
@@ -1,1 +1,62 @@
 
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["html", "nog te leren"],
+      datasets: [
+        {
+          label: "HTML",
+          backgroundColor: ["#4c8a7b", "#abdacb",],
+          data: [80,20,]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'HTML'
+      }
+    }
+});
+
+
+new Chart(document.getElementById("doughnut-chart2"), {
+    type: 'doughnut',
+    data: {
+      labels: ["css", "nog te leren"],
+      datasets: [
+        {
+          label: "Css",
+          backgroundColor: ["#4c8a7b", "#abdacb",],
+          data: [80,20,]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Css'
+      }
+    }
+});
+
+
+new Chart(document.getElementById("doughnut-chart3"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Javascript", "nog te leren"],
+      datasets: [
+        {
+          label: " javascript",
+          backgroundColor: ["#4c8a7b", "#abdacb",],
+          data: [60,40,]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Javascript'
+      }
+    }
+});
